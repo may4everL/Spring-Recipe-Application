3c8111302fef8eb89ff89da74fb902afc007c825
may4everL <weis9004@gmail.com>
2022-03-28 18:00:30 +0800

    -Initial Commit

---
+++ src/test/java/wei/springFramework/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package wei.springFramework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
